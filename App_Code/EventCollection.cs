3e0eab677c2fe75f9ae325d3918bfacea7db38c6
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-24 17:26:25 +1300

    Event(1)

---
+++ App_Code/EventCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for EventCollection
+/// </summary>
+
+public class EventDescription
+{
+    public EventSource Source { get; set; }
+    public string Type { get; set; }
+}
+public enum EventSource
+{
+    Application,
+    Page,
+    MasterPage,
+    Control
+}
+public class EventCollection
+{
+    private static List<EventDescription> events = new List<EventDescription>();
+    public static void Add(EventSource level, string type)
+    {
+        events.Add(new EventDescription { Source = level, Type = type });
+        System.Diagnostics.Debug.WriteLine("Event: {0}, {1}", level, type);
+    }
+    public static IEnumerable<EventDescription> Events
+    {
+        get { return events; }
+    }
+}
