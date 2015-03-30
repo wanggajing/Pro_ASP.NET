f6d651e6c7910d81c71f38dcb25c101986ea0583
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-30 14:36:06 +1300

    Restrict access,Timing request,logging response

--- App_Code/Global.asax.cs
+++ App_Code/Global.asax.cs
@@ -8,84 +8,38 @@ using System.Web;
 /// </summary>
 public class Global : System.Web.HttpApplication
 {
-    public Global()
-    {
-        BeginRequest += HandleEvent;
-    }
-    protected void HandleEvent(object sender, EventArgs e)
-    {
-        switch (Context.CurrentNotification)
-        {
-            case RequestNotification.BeginRequest:
-                EventCollection.Add(EventSource.Application, "BeginRequest");
-                if (Request.RawUrl == "/Time") 
-                {
-                    Response.Write(Context.Timestamp.ToLongTimeString());
-                    CompleteRequest();
-                }
-                break;
-            default:
-                string eventName = Context.CurrentNotification.ToString();
-                EventCollection.Add(EventSource.Application, eventName);
-                break;
-        }
-    }
+    private DateTime startTime;
     protected void Application_Start(object sender, EventArgs e)
     {
         EventCollection.Add(EventSource.Application, "Start");
         Application["message"] = "Application Events";
-        CreateTimeStamp();
     }
     protected void Application_End(object sender, EventArgs e)
     {
         EventCollection.Add(EventSource.Application, "End");
     }
-    protected void CreateTimeStamp()
+    protected void Application_BeginRequest(object sender, EventArgs e)
     {
-//        This works for the instances of the Global object that are instantiated to handle the request lifecycle, but not for
-//those instances created for the application lifecycle and we cause an HttpException when we call CreateTimeStamp
-//from the Application_Start method.
-        //Session property will return null until the PostAcquireRequestState application event has been triggered. 
-        //Session property in HttpApplication Returns the HttpContext.Session value, but throws an HttpException if the value is null.
-        string stamp = Context.Timestamp.ToLongTimeString();
-        if (Context.Session != null)
+        startTime = Context.Timestamp;
+    }
+    protected void Application_EndRequest(object sender, EventArgs e)
+    {
+        double elapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+        System.Diagnostics.Debug.WriteLine(
+        string.Format("Duration: {0} {1}ms", Request.RawUrl, elapsed));
+    }
+    protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+    {
+        if (Request.Url.LocalPath == "/Chaptor13/Params.aspx" &&
+        !User.Identity.IsAuthenticated)
         {
-            Session["request_timestamp"] = stamp;
-        }
-        else
-        {
-            Application["app_timestamp"] = stamp;
+            Context.AddError(new UnauthorizedAccessException());
         }
     }
-    //protected void Application_BeginRequest(object sender, EventArgs e)
-    //{
-    //    EventCollection.Add(EventSource.Application, "BeginRequest");
-    //    Response.Write(string.Format("Request started at {0}",
-    //    DateTime.Now.ToLongTimeString()));
-    //}
-    //protected void Application_EndRequest(object sender, EventArgs e)
-    //{
-    //    EventCollection.Add(EventSource.Application, "EndRequest");
-    //}
-
-    //void Application_Error(object sender, EventArgs e)
-    //{
-    //    // Code that runs when an unhandled error occurs
-
-    //}
-
-    //void Session_Start(object sender, EventArgs e)
-    //{
-    //    // Code that runs when a new session is started
-
-    //}
-
-    //void Session_End(object sender, EventArgs e)
-    //{
-    //    // Code that runs when a session ends. 
-    //    // Note: The Session_End event is raised only when the sessionstate mode
-    //    // is set to InProc in the Web.config file. If session mode is set to StateServer 
-    //    // or SQLServer, the event is not raised.
-
-    //}
+    protected void Application_LogRequest(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(
+        string.Format("Request for {0} - code {1}",
+        Request.RawUrl, Response.StatusCode));
+    }
 }
